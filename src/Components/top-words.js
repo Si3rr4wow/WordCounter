70dd41bdb12d69b61758b2c79a701ee14a39f13c
Mark <si3rr4wow@gmail.com>
2020-12-12 12:09:06 +0000

    Display the array as requested.

--- src/Components/top-words.js
+++ src/Components/top-words.js
@@ -4,16 +4,28 @@ import PropTypes from 'proptypes'
 import { sortTopWords } from 'Utils'
 
 const TopWords = ({ topWords }) => {
+  const sortedTopWords = sortTopWords(topWords)
+  const topWordsArray = sortedTopWords.map(({ word }) => (
+    word
+  ))
   return (
     <>
       <h3>
-        Top Words:
+        Top Words With Counts:
       </h3>
       {
-        sortTopWords(topWords).map(({ word, count }) =>
+        sortedTopWords.map(({ word, count }) =>
           <p key={word}>{word}: {count}</p>
         )
       }
+      <h3>
+        Top Words As an Array:
+      </h3>
+      <code>
+      {
+        JSON.stringify(topWordsArray)
+      }
+      </code>
     </>
   )
 }
